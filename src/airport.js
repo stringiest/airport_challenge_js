a517f7927cc0d56c07e937b28005bc7ef51210da
Lucy Stringer <lucyjstringer@gmail.com>
2020-12-02 11:52:46 +0000

    translation of challenge into JS from Ruby plus add README

---
+++ src/airport.js
@@ -0,0 +1,26 @@
+'use strict';
+
+class Airport{
+  constructor(weather) {
+    this._weather = typeof weather !== 'undefined' ? weather : new Weather();
+    this._hangar = []
+  }
+  planes() {
+    return this._hangar;
+  }
+  clearForLanding(plane) {
+    if(this._weather.isStormy()) {
+      throw new Error('cannot land during storm');
+    }
+    this._hangar.push(plane);
+  };
+  clearForTakeOff(plane) {
+    if(this._weather.isStormy()) {
+      throw new Error('cannot takeoff during storm');
+    }
+    this._hangar = [];
+  }
+  isStormy() {
+    return false;
+  }
+};
